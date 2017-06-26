a83264b581ef70f5d4b0e155366840333a2d4e37
Maelle <maelle.taurand@ac-nantes.fr>
2017-06-26 17:51:39 +0200

    c buggé

--- Taurand Maelle/mission4/server.js
+++ Taurand Maelle/mission4/server.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const app = express();
 
+let mesEleves = [];
+
 const Eleve = require("./eleve.js");
-var unEleve = new Eleve(nom='toto',prenom='titi');
+let unEleve = new Eleve(id= mesEleves.length +1, nom='toto',prenom='titi');
+mesEleves.push(unEleve);
+
+let unEleve2 = new Eleve(id= mesEleves.length +1, nom='valery',prenom='titi');
+mesEleves.push(unEleve2);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/eleves',(req,res) =>  {
+mesEleves.forEach(
+      (unEleve) => {res.send(`Hello Eleve ${mesEleves[id-1].nom}`);}
+	  	);
+});
 
 
 app.get('/eleve/:id', (req, res) => {
   let id=req.params.id;
-  if (id == 1) {
-    res.send('Hello Eleve ' + unEleve.nom);
+  let trouve = false;
+
+  if (id < mesEleves.length){
+    res.send(`Hello Eleve ${mesEleves[id-1].nom}`);
+	  
+  }	
+  else 
+  {
+    res.status(404).send("Sorry! User doesn't exist");
   }
 });
+
 
 try {
     app.listen(8081,'127.0.0.1',() => {
