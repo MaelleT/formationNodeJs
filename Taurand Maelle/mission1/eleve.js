36b07e46d3e4b94365eea72a701b477d0b3c034b
Maelle <maelle.taurand@ac-nantes.fr>
2017-06-26 11:13:36 +0200

    classe eleve

--- Taurand Maelle/mission1/eleve.js
+++ Taurand Maelle/mission1/eleve.js
@@ -1,1 +1,35 @@
-console.log("Hello world");
+// auteur : maelle
+
+function Eleve(nom = 'nom',prenom = 'prenom') {
+	this._nom = nom;
+	this._prenom = prenom;
+	//console.log('Je m\'appelle ' + this._nom + ' ' + this._prenom);
+
+};
+
+/*methode de la classe Eleve*/
+Eleve.prototype.getNom= function() {
+	return this._nom;
+};
+
+/*methode de la classe Eleve*/
+Eleve.prototype.getPrenom= function() {
+	return this._nom;
+};
+
+/*methode de la classe Eleve*/
+Eleve.prototype.setNom= function(unNom) {
+	this._nom = unNom;
+};
+
+Eleve.prototype.setPrenom= function(unNom) {
+	this._nom = unNom;
+};
+
+
+
+var eleve = new Eleve(nom='Taurand',prenom='Maelle');
+//eleve.get_nom();
+console.log(eleve.getNom());
+eleve.setNom(nom='Truc');
+console.log(eleve.getNom());
