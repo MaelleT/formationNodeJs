44cc182e5c692f9eee09a27395516e4840a651c1
Maelle <maelle.taurand@ac-nantes.fr>
2017-06-26 16:31:48 +0200

    principe de arrow

--- Taurand Maelle/mission4/server.js
+++ Taurand Maelle/mission4/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
  
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
  
-app.listen(8081);
+app.get('/eleve', (req, res) => {
+  res.send('Hello Eleve');
+});
+
+try {
+    app.listen(8081,'127.0.0.1',() => {
+        console.log("Serveur OK");
+    }); //démarrage du serveur sur le port 8081
+} catch (err) {
+	console.error(' Erreur de démarrage du serveur!', err);
+}
