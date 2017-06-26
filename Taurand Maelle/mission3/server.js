8377320ebc0f66a28d7c879b53fca038c554f9cc
Maelle <maelle.taurand@ac-nantes.fr>
2017-06-26 15:57:42 +0200

    execution commande npm init

--- Taurand Maelle/mission3/server.js
+++ Taurand Maelle/mission3/server.js
@@ -1,4 +1,12 @@
+const express=require("express");
+const app = express();
+
+app.listen(8081,()=> {
+        console.log('il y a eu une requête !!!!');
+})
+
 const http = require('http');
+
 
 const server = http.createServer(function(req,res){
 
@@ -9,7 +17,11 @@ const server = http.createServer(function(req,res){
 }
 )
 
-server.listen(8081,'127.0.0.1',function (){
 
+try {
+    server.listen(8081,'127.0.0.1',function (){
+
+    }); //démarrage du serveur sur le port 8081
+} catch (err) {
+	console.error(' Erreur de démarrage du serveur!', err);
 }
-);
