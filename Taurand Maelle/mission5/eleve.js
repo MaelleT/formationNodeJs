6e2390df51074162b6bd98be697b14b06261ba60
Maelle <maelle.taurand@ac-nantes.fr>
2017-06-27 12:13:20 +0200

    Ajout d'un formulaire élève + POSt + GET

---
+++ Taurand Maelle/mission5/eleve.js
@@ -0,0 +1,65 @@
+// auteur : maelle
+const Note = require('./note.js');
+const Cours = require('./cours.js');
+
+class Eleve {
+	
+	constructor(id = 'id', nom = 'nom',prenom = 'prenom') {
+		this._id = id;
+		this._nom = nom;
+		this._prenom = prenom;
+		//console.log('Je m\'appelle ' + this._nom + ' ' + this._prenom);
+		this._mesNotes = [];
+	};
+	get id() {
+		return this._id;
+	}
+
+	/*methode de la classe Eleve*/
+	get nom() {
+		return this._nom;
+	};
+	/*methode de la classe Eleve*/
+	set nom(unNom) {
+		this._nom = unNom;
+	};
+	/*methode de la classe Eleve*/
+	get prenom() {
+		return this._prenom;
+	};
+
+	set prenom(unPrenom) {
+		this._prenom = unPrenom;
+	};
+
+	ajouterNoteMatiere(valeur,uneMatiere){
+	
+		let note = new Note(valeur,uneMatiere);
+		this._mesNotes.push(note);
+	}
+
+	afficherNotes(display){
+	
+		this._mesNotes.forEach(function(uneNote) {
+		display(uneNote.getValeur());
+		});		
+	}
+
+	calculerMoyenneMatiere(unCours){
+	
+		let tot = 0;
+		let nb = 0;
+		this._mesNotes.forEach(function(uneNote) {
+				console.log(uneNote.getCours().getLibelle());
+				if (uneNote.getCours().getLibelle() == unCours.getLibelle()){
+					tot+=uneNote.getValeur();
+					nb +=1; 
+					console.log(nb);
+				}
+		});	
+		
+		return tot/nb;
+	}
+}
+module.exports = Eleve;
+
---
+++ Taurand Maelle/mission5/server.js
@@ -0,0 +1,99 @@
+//modules node
+const express = require('express');
+//const redirect = require('express-redirect');
+
+const bodyParser = require('body-parser');
+
+
+
+
+//modules "métiers"
+const Eleve = require("./eleve.js");
+
+//initialisation de l'application
+const app = express();
+//redirect(app);
+
+//ajouter pour le post
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+let mesEleves = [];
+
+let unEleve = new Eleve(id=1, nom='Tschaen',prenom='Valery');
+mesEleves.push(unEleve);
+
+let unEleve2 = new Eleve(id=2, nom='Taurand',prenom='Maelle');
+mesEleves.push(unEleve2);
+
+
+app.get('/', (req, res) => {
+  let responseText = 'Bienvenue dans l\'application de gestion des élèves';
+  res.send(responseText);
+});
+
+//affiche tous les élèves
+app.get('/eleves',(req,res) =>  {
+      let responseText = 'Voici la liste des élèves </br>';
+      mesEleves.forEach(
+      (unEleve) => {responseText += `Elève ${unEleve.prenom} ${unEleve.nom}  </br>`;}
+	  	);
+        res.send(responseText);
+
+  
+});
+
+//affiche un élève
+app.get('/eleve/:id', (req, res) => {
+  let id=req.params.id;
+  let trouve = false;
+
+  if (id < mesEleves.length+1){
+    let responseText = `Hello Eleve ${mesEleves[id-1].nom}`;
+    res.send(responseText);
+
+  }	
+  else 
+  {
+    res.status(404).send("Sorry ! User doesn't exist");
+  }
+});
+
+//
+app.get('/eleve',(req,res) => {
+
+  let responseText = '<form action="/eleve" method="post">';
+  responseText += 'Nom :<br>';
+  responseText += '<input type="text" name="nom" value="Mickey"><br>';
+  responseText += 'Prenom:<br>';
+  responseText += '<input type="text" name="prenom" value="Mouse"><br><br>';
+  responseText += '<input type="submit" value="Envoyer">';
+  responseText += '</form>';
+  res.send(responseText);
+ 
+});
+
+//Ajoute un eleve
+app.post('/eleve', (req, res) => {
+  let eleve_nom = req.body.nom;
+  let eleve_prenom = req.body.prenom;
+ 
+  console.log(eleve_nom);
+  mesEleves.push(new Eleve(id=mesEleves.length+1, nom=eleve_nom, prenom=eleve_prenom));
+  let responseText = "Eleve ajouté";
+  res.send(responseText);
+  //app.redirect('/eleves');
+       
+});
+
+
+//APP
+
+try {
+    app.listen(8081,'127.0.0.1',() => {
+        console.log("Serveur OK");
+    }); //démarrage du serveur sur le port 8081
+} catch (err) {
+	console.error(' Erreur de démarrage du serveur!', err);
+}
